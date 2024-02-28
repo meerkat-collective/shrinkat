ddb9b3349db552c31e01198eed59bd79ee95a7c0
dante <jamesmidzi@gmail.com>
2024-02-28 18:30:01 +0200

    fix: button fix

--- app/src/components/ui/Button.svelte
+++ app/src/components/ui/Button.svelte
@@ -5,7 +5,7 @@
 <button
 	on:click
 	{...$$restProps}
-	class={`py-2 px-4 w-full bg-kat-500 rounded text-slate-200 hover:bg-kat-300 transition duration-300 ${classes}`}
+	class={`py-2 px-4 w-full bg-kat-400 rounded text-slate-200 hover:bg-kat-300 transition duration-300 ${classes}`}
 >
 	<slot />
 </button>
